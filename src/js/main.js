cd036ae503c9860fd03b987a1b51a2fe74f91b94
Aleksandr <hiphopkrv@gmail.com>
2016-10-04 20:20:01 +0300

    add search

--- src/js/main.js
+++ src/js/main.js
@@ -7,16 +7,25 @@
         window.open(this.randomArticleurl, "_blank");
     },
 
-    search: function() {
-        $.getJSON(this.playListURL, function (data) {
+    search: function(titles) {
+        $.getJSON('http://en.wikipedia.org/w/api.php?format=json&action=query&titles=' + titles + '&prop=revisions&rvprop=content&callback=?', function(data) {
             var hash = data;
             var page_value = "";
-            $.each(data["query"]["pages"], function (k, v) {
-                console.log(k);
-                $.each(v, function (key, val) {
-                    console.log(key);
-                });
+            $.each(data["query"]["pages"], function(k, v) {
+                appendData(k, v);
             });
         });
     }
 };
+
+function appendData(k, v) {
+    var target = $(".results");
+    target.empty();
+    target.append('<li>' + 'key=' + k + 'val=' + v + '</li>');
+}
+$(document).ready(function() {
+    $(".search").on('click', function() {
+        var titles = $("input").val();
+        mgr.search(titles)
+    });
+});
