605242ed42632e528a9854dfaf8c84270d5f7226
Aleksandr <hiphopkrv@gmail.com>
2017-02-03 16:25:31 +0200

    parse result from API

---
+++ src/js/alg.js
@@ -0,0 +1,151 @@
+﻿function orbitalPeriod(arr) {
+    var GM = 398600.4418;
+    var earthRadius = 6367.4447;
+    var result = [];
+
+    arr.forEach(function (element) {
+        var name = element.name;
+        var avgAlt = element.avgAlt;
+        var orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow((earthRadius + avgAlt), 3) / GM));
+
+        result.push({ name: name, orbitalPeriod: orbitalPeriod });
+    });
+
+    return result;
+}
+
+function pairwise(arr, arg) {
+    return arr.reduce(function (prev, cur, index) {
+
+    });
+}
+
+
+function truthCheck(collection, pre) {
+    var collectionLength = collection.length;
+    var matches = 0;
+    collection.forEach(function (element) {
+        if (element.hasOwnProperty(pre)) {
+            if (element[pre]) {
+                matches++;
+            }
+        }
+    });
+    return collectionLength === matches;
+}
+
+truthCheck([
+    { "user": "Tinky-Winky", "sex": "male" },
+    { "user": "Dipsy", "sex": "male" },
+    { "user": "Laa-Laa", "sex": "female" },
+    { "user": "Po", "sex": "female" }], "sex");
+
+
+//Create a function that sums two arguments together. If only one argument is provided, 
+//then return a function that expects one argument and returns the sum.
+//For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+//Calling this returned function with a single argument will then return the sum:
+//var sumTwoAnd = addTogether(2);
+//    sumTwoAnd(3) returns 5.
+//    If either argument isn't a valid number, return undefined.
+
+window.addTogether = function () {
+    var args = Array.prototype.slice.call(arguments);
+    if (arguments.length === 2) {
+        if (typeof arguments[0] !== "number") {
+            return undefined;
+        }
+        if (typeof arguments[1] !== "number") {
+            return undefined;
+        }
+        return arguments[0] + arguments[1];
+    }
+
+    if (arguments.length === 1) {
+        if (typeof arguments[0] !== "number") {
+            return undefined;
+        }
+        return function (num) {
+            if (typeof num !== "number") {
+                return undefined;
+            }
+            return args[0] + num;
+        }
+    }
+}
+
+//Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
+//The second argument, func, is a function you'll use to test the first elements of the array to decide if you should drop it or not.
+//Return the rest of the array, otherwise return an empty array.
+
+window.dropElements = function (arr, func) {
+    var dropped = false;
+    var ind = arr.forEach(function (element, index) {
+        if (!func(element)) {
+            return index;
+        }
+    });
+
+    var re = arr.slice(ind);
+
+    return dropped ? arr : [];
+}
+
+dropElements([1, 2, 3], function (n) { return n < 3; });
+
+//dropElements([1, 2, 3, 4], function(n) {return n >= 3;}) should return [3, 4].
+//dropElements([0, 1, 0, 1], function(n) {return n === 1;}) should return [1, 0, 1].
+//dropElements([1, 2, 3], function(n) {return n > 0;}) should return [1, 2, 3].
+//dropElements([1, 2, 3, 4], function(n) {return n > 5;}) should return [].
+//dropElements([1, 2, 3, 7, 4], function(n) {return n > 3;}) should return [7, 4].
+//dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}) should return [3, 9, 2].
+
+
+//Make a function that looks through an array of objects (first argument) and returns an array 
+//    of all objects that have matching property and value pairs (second argument). 
+//Each property and value pair of the source object has to be present in the object from the collection
+//if it is to be included in the returned array.
+
+//For example, if the first argument is
+//[{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }],
+//and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument),
+//because it contains the property and its value, that was passed on as the second argument.
+
+window.whatIsInAName = function (collection, source) {
+    var resultArray = [];
+
+    var sourceObjKeys = Object.keys(source);
+
+    collection.forEach(function (collectionObj) {
+        if (sourceObjKeys.every(function(sourceKey) {
+            return collectionObj.hasOwnProperty(sourceKey) && collectionObj[sourceKey] === source[sourceKey];
+        })) {
+            resultArray.push(collectionObj);
+        }
+    });
+
+    return resultArray;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+window.smallestCommons = function (arr) {
+    var lowest = function(a, b) {
+        return a > b ? b : a;
+    }
+    var highest = function (a, b) {
+        return a > b ? a : b;
+    }
+    var numbersForSc = [];
+    for (var i = lowest(arr[0], arr[1]); i <= highest(arr[0], arr[1]); i++) {
+        numbersForSc.push(i);
+    }
+
+
+
+    return arr;
+}
+
+
+smallestCommons([1, 5]);
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,142 @@
-﻿
+﻿/**
+    @name: angular-wikipedia-api-factory 
+    @version: 0.2.3 (19-06-2016) 
+    @author: Jonathan Hornung 
+    @url: https://github.com/JohnnyTheTank/angular-wikipedia-api-factory#readme 
+    @license: MIT
+*/
+"use strict";
 
-mediaWikiUrl = "https://en.wikipedia.org/w/api.php",
-playListUrl = "http://en.wikipedia.org/w/api.php?format=json&action=query&titles=India&prop=revisions&rvprop=content&callback=?"
+angular.module("jtt_wikipedia", [])
+    .factory('wikipediaFactory', ['$http', 'wikipediaSearchDataService', function ($http, wikipediaSearchDataService) {
 
-var app = angular.module('WikipediaViewer', ['jtt_wikipedia']);
+        var wikipediaFactory = {};
+
+        wikipediaFactory.searchArticlesByTitle = function (_params) {
+
+            var wikipediaSearchData = wikipediaSearchDataService.getNew("searchArticlesByTitle", _params);
+
+            return $http.jsonp(
+                wikipediaSearchData.url,
+                {
+                    method: 'GET',
+                    params: wikipediaSearchData.object,
+                }
+            );
+        };
+
+        wikipediaFactory.searchArticles = function (_params) {
+
+            var wikipediaSearchData = wikipediaSearchDataService.getNew("searchArticles", _params);
+
+            return $http.jsonp(
+                wikipediaSearchData.url,
+                {
+                    method: 'GET',
+                    params: wikipediaSearchData.object,
+                }
+            );
+        };
+
+        wikipediaFactory.getArticle = function (_params) {
+
+            var wikipediaSearchData = wikipediaSearchDataService.getNew("getArticle", _params);
+
+            return $http.jsonp(
+                wikipediaSearchData.url,
+                {
+                    method: 'GET',
+                    params: wikipediaSearchData.object,
+                }
+            );
+        };
+
+        return wikipediaFactory;
+    }])
+    .service('wikipediaSearchDataService', function () {
+        this.getApiBaseUrl = function (_lang) {
+            return 'https://' + _lang + ".wikipedia.org/w/api.php";
+        };
+
+        this.fillDataInObjectByList = function (_object, _params, _list) {
+
+            angular.forEach(_list, function (value, key) {
+                if (angular.isDefined(_params[value])) {
+                    _object.object[value] = _params[value];
+                }
+            });
+
+            return _object;
+        };
+
+        this.getNew = function (_type, _params) {
+
+            var wikipediaSearchData = {
+                object: {
+                    callback: "JSON_CALLBACK",
+                    action: 'query',
+                    format: 'json',
+                    formatversion: 2,
+                },
+                url: "",
+            };
+
+            if (angular.isUndefined(_params.lang)) {
+                _params.lang = 'en'
+            }
+
+            if (angular.isUndefined(_params.pithumbsize)) {
+                _params.pithumbsize = '400'
+            }
+
+            switch (_type) {
+                case "searchArticlesByTitle":
+                    wikipediaSearchData.object.prop = 'extracts|pageimages|info';
+                    wikipediaSearchData.object.generator = 'search';
+                    wikipediaSearchData.object.gsrsearch = 'intitle:' + _params.term;
+                    wikipediaSearchData.object.pilimit = 'max';
+                    wikipediaSearchData.object.exlimit = 'max';
+                    wikipediaSearchData.object.exintro = '';
+
+                    wikipediaSearchData = this.fillDataInObjectByList(wikipediaSearchData, _params, [
+                        'prop', 'generator', 'gsrsearch', 'pilimit', 'exlimit', 'exintro', 'rvparse', 'formatversion', 'prop', 'pithumbsize', 'gsrlimit'
+                    ]);
+                    wikipediaSearchData.url = this.getApiBaseUrl(_params.lang);
+                    break;
+
+                case "searchArticles":
+                    wikipediaSearchData.object.prop = 'extracts|pageimages|info';
+                    wikipediaSearchData.object.generator = 'search';
+                    wikipediaSearchData.object.gsrsearch = _params.term;
+                    wikipediaSearchData.object.pilimit = 'max';
+                    wikipediaSearchData.object.exlimit = 'max';
+                    wikipediaSearchData.object.exintro = '';
+
+                    wikipediaSearchData = this.fillDataInObjectByList(wikipediaSearchData, _params, [
+                        'prop', 'generator', 'gsrsearch', 'pilimit', 'exlimit', 'exintro', 'rvparse', 'formatversion', 'prop', 'pithumbsize', 'gsrlimit'
+                    ]);
+                    wikipediaSearchData.url = this.getApiBaseUrl(_params.lang);
+                    break;
+
+                case "getArticle":
+                    wikipediaSearchData.object.prop = 'extracts|pageimages|images|info';
+                    wikipediaSearchData.object.titles = _params.term;
+
+                    wikipediaSearchData = this.fillDataInObjectByList(wikipediaSearchData, _params, [
+                        'prop', 'rvparse', 'formatversion', 'prop', 'pithumbsize'
+                    ]);
+                    wikipediaSearchData.url = this.getApiBaseUrl(_params.lang);
+                    break;
+            }
+            return wikipediaSearchData;
+        };
+    });
+
+var app = angular.module('WikipediaViewer', ['ngRoute', 'ngResource', 'jtt_wikipedia']);
+
+app.config(function ($sceProvider) {
+    $sceProvider.enabled(false);
+});
 
 app.controller('WikipediaViewerController', [
     '$scope', '$http', '$window', 'wikipediaFactory', function ($scope, $http, $window, wikipediaFactory) {
@@ -17,9 +150,20 @@ app.controller('WikipediaViewerController', [
         }
 
         $scope.search = function () {
-            $scope.searchResults = wikipediaFactory.getArticle({
-                term: $scope.searchString
+            var apiUrl = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+            var page = 'https://en.wikipedia.org/?curid=';
+
+            $scope.searchResults = [];
+
+            $http.jsonp(apiUrl + $scope.searchString, { jsonpCallbackParam: 'callback' })
+            .then(function (response) {
+                var data = response.data;
+                var searchResults = data.query.pages;
+                angular.forEach(searchResults, function (v, k) {
+                    $scope.searchResults.push({ title: v.title, body: v.extract, page: page + v.pageid, thumbnail : v.thumbnail.source });
+                });
             });
         }
+
     }
 ]);
